946db3b82906d8697d263a313a2747e0510b2a5c
Felipe Mendes <felipemendesdev@gmail.com>
2023-01-04 08:20:09 -0300

    Site and authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::prefix('admin')->namespace('Admin')->group(function(){
+Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function(){
 
 
     //Plans x Profiles
@@ -25,11 +25,11 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
     Route::any('profiles/search', 'ACL\ProfileController@search')->name('profiles.search');
     //Plans
     Route::delete('plans/{url}/details/{idDetail}', 'DetailPlanController@destroy')->name('details.plan.destroy');
+    Route::get('plans/{url}/details/create', 'DetailPlanController@create')->name('details.plan.create');
     Route::get('plans/{url}/details/{idDetail}', 'DetailPlanController@show')->name('details.plan.show');
     Route::put('plans/{url}/details/{idDetail}', 'DetailPlanController@update')->name('details.plan.update');
     Route::get('plans/{url}/details/{idDetail}/edit', 'DetailPlanController@edit')->name('details.plan.edit');
     Route::post('plans/{url}/details', 'DetailPlanController@store')->name('details.plan.store');
-    Route::get('plans/{url}/details/create', 'DetailPlanController@create')->name('details.plan.create');
     Route::get('plans/{url}/details', 'DetailPlanController@index')->name('details.plan.index');
 
 
@@ -43,3 +43,11 @@ Route::prefix('admin')->namespace('Admin')->group(function(){
     Route::get('plans', 'PlanController@index')->name('plans.index');
     Route::get('/', 'PlanController@index')->name('admin.index');
 });
+
+
+Route::get('/', 'Site\SiteController@index')->name('site.home');
+
+/**
+ * Auth Routes
+ */
+Auth::routes();
